fee0e6dc0c0dcb6de2a4828fca7166eb9e88017d
RoyCook <lh1104513635@163.com>
2018-05-08 11:05:22 +0800

    create user with promise

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -3,13 +3,12 @@
 const mongoose = require('mongoose'),
     User = mongoose.model('User');
 
-exports.create = async (req, res, next) => {
+exports.create = (req, res, next) => {
     let user = new User(req.body);
 
-    try {
-        user = await user.save();
-        return res.json(user);
-    } catch (err) {
-        return next(err);
-    } 
+    user.save()
+        .then((user) => {
+            return res.json(user);
+        })
+        .catch(next);
 };

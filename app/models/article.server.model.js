9fb7b94860863f52e7833cdaef1edbf13da42c9a
RoyCook <lh1104513635@163.com>
2018-05-17 12:06:03 +0800

    add article model

---
+++ app/models/article.server.model.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+let ArticleSchema = new Schema({
+    title: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Title can not be blank'
+    },
+    content: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    creator: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    }
+}, {
+    timeStamp: {
+        createdAt: 'created',
+        updatedAt: 'updated'
+    }
+});
+
+mongoose.model('Article', ArticleSchema, 'articles');

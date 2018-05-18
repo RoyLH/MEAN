3915a5661c3adb4e9a161c5b44b28851f39465d7
RoyCook <lh1104513635@163.com>
2018-05-18 18:25:38 +0800

    require chat controller

--- config/socketio.js
+++ config/socketio.js
@@ -33,6 +33,6 @@ module.exports = () => {
     });
 
     io.on('connection', function (socket) {
-        /* */
+        require('../app/controllers/chat.server.controller')(io, socket);
     });
 };

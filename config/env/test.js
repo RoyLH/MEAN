ff9dda27de7c839d738e805201fa242419262897
RoyCook <lh1104513635@163.com>
2018-05-20 12:49:31 +0800

    add test environment config

---
+++ config/env/test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+    db: 'mongodb://localhost/mean-test',
+    sessionSecret: 'testSessionSecret',
+    facebook: {
+        clientID: 'facebookClientID',
+        clientSecret: 'facebookClientSecret',
+        callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+    },
+    twitter: {
+        consumerKey: 'tybP8KZMI5Qer0LIpES3vLMva',
+        consumerSecret: 'ZJDVmyNdmRtkkN1cnRZSVVaN4vgqV1c1j5nb2rd8MdvVmcxltu',
+        callbackURL: 'http://localhost:3000/oauth/twitter/callback'
+    },
+    google: {
+        clientID: '361041994869-v5vl8amkgtbfg493meps1nmk52augacp.apps.googleusercontent.com',
+        clientSecret: '2k0IT3Vdrh1QXN6Q0y8CruEa',
+        callbackURL: 'http://localhost:3000/oauth/google/callback'
+    }
+};

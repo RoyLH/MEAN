7fe6ec37ccea7be021a0bb1ed0445611fa4fd4dd
liuheng <liuheng@yidian-inc.com>
2018-05-28 15:50:37 +0800

    change example to core in templateUrl

--- public/core/config/core.client.routes.js
+++ public/core/config/core.client.routes.js
@@ -3,7 +3,7 @@
 angular.module('core').config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         .when('/', {
-            templateUrl: '/core/views/example.client.view.html'
+            templateUrl: '/core/views/core.client.view.html'
         })
         .otherwise({
             redirectTo: '/'
